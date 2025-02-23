8f1b7e66a7a34a9a4818e6ef0ee2a7128326c442
Agostinho Ramos <agostinhopina095@gmail.com>
2025-02-23 11:24:42 +0000

    Improve format text button

--- src/noaapp/common/views/welcome/index.tsx
+++ src/noaapp/common/views/welcome/index.tsx
@@ -4,15 +4,35 @@ const WelcomeView = () => {
     const [notes, setNotes] = useState("")
     const [htmlContent, setHtmlContent] = useState("")
     const textareaRef = useRef(null)
+    const pastFormatToggleButton = useRef<HTMLButtonElement>(null)
+    const [enablePlainTextFormat, setEnablePlainTextFormat] = useState(false)
 
     useEffect(() => {
         setHtmlContent("")
     }, [notes])
 
     const handlePaste = (event: React.ClipboardEvent<HTMLDivElement>) => {
-        event.preventDefault()
-        const text: string = event.clipboardData.getData("text/plain")
-        document.execCommand("insertText", false, text)
+        if(!enablePlainTextFormat){
+            event.preventDefault()
+            const text: string = event.clipboardData.getData("text/plain")
+            document.execCommand("insertText", false, text)
+        }
+    }
+
+    const handlePastFormatToggleButton = () => {
+        if(pastFormatToggleButton.current){
+            if(pastFormatToggleButton.current.classList.contains("enabled")){
+                pastFormatToggleButton.current.classList.remove("bg-blue-500/10")
+                pastFormatToggleButton.current.classList.remove("text-blue-500")
+                pastFormatToggleButton.current.classList.remove("enabled")
+                setEnablePlainTextFormat(false)
+            }else{
+                pastFormatToggleButton.current.classList.add("bg-blue-500/10")
+                pastFormatToggleButton.current.classList.add("text-blue-500")
+                pastFormatToggleButton.current.classList.add("enabled")
+                setEnablePlainTextFormat(true)
+            }
+        }
     }
 
     return (
@@ -60,6 +80,15 @@ const WelcomeView = () => {
 
                             <div className="flex items-center justify-between mt-2 p-2 pt-0 rounded-lg">
                                 <div className="flex gap-2">
+                                    <button 
+                                        type="button" ref={pastFormatToggleButton} 
+                                        onClick={handlePastFormatToggleButton} 
+                                        className='flex items-center justify-center h-9 rounded-full border w-9'
+                                        >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                                        </svg>
+                                    </button>
                                     <button type="button" className='flex items-center justify-center h-9 rounded-full border w-9'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
