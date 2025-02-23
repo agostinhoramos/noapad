5c80003c575c78be3737ce3aab383e0a2507afc0
Agostinho Ramos <agostinhopina095@gmail.com>
2025-02-23 10:45:24 +0000

    Setting up real UI

--- src/noaapp/common/views/welcome/index.tsx
+++ src/noaapp/common/views/welcome/index.tsx
@@ -6,15 +6,14 @@ const WelcomeView = () => {
     const textareaRef = useRef(null)
 
     useEffect(() => {
-        if (textareaRef.current) {
-            textareaRef.current.style.height = "auto"
-            textareaRef.current.style.height = textareaRef.current.scrollHeight + "px"
-        }
+        setHtmlContent("")
     }, [notes])
 
-    useEffect(() => {
-        setHtmlContent("<strong>Welcome to NoaPad!</strong> This is an example of dynamically loaded HTML content.<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1")
-    }, [])
+    const handlePaste = (event: React.ClipboardEvent<HTMLDivElement>) => {
+        event.preventDefault()
+        const text: string = event.clipboardData.getData("text/plain")
+        document.execCommand("insertText", false, text)
+    }
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-100">
@@ -26,12 +25,12 @@ const WelcomeView = () => {
                 />
                 <div className="flex ml-1">
                     <button type="button" className='flex items-center justify-center h-9 w-9 text-gray-800 bg-white border rounded-full'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                         </svg>
                     </button>
                     <button type="button" className='flex items-center justify-center ml-1 h-9 text-gray-800'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                         </svg>
                     </button>
@@ -47,8 +46,9 @@ const WelcomeView = () => {
 
                             <div
                                 ref={textareaRef}
-                                className="w-full flex-1 p-2 rounded-lg focus:outline-none resize-none overflow-auto todo-max-h-72 min-h-32 relative"
+                                className="w-full flex-1 py-2 px-3 rounded-lg focus:outline-none resize-none overflow-auto todo-max-h-72 min-h-32 relative"
                                 contentEditable={true}
+                                onPaste={handlePaste}
                                 onInput={(e) => setNotes(e.currentTarget.innerHTML)}
                                 data-placeholder="Write your notes here..."
                                 style={{ position: 'relative' }}
@@ -74,6 +74,7 @@ const WelcomeView = () => {
                                     </button>
                                 </div>
                             </div>
+
                         </div>
 
                     </div>
@@ -81,6 +82,6 @@ const WelcomeView = () => {
             </div>
         </div>
     )
-} 
+}
 
 export default WelcomeView
